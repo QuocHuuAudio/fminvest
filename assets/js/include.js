c0328c74261688fd5b317de868280fcd10b78401
QuocHuuAudio <127771347+QuocHuuAudio@users.noreply.github.com>
2023-07-07 20:47:20 +0700

    include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -12,17 +12,18 @@ const fetchContent = (url) => {
    });
 };
 
-Promise.all([
-   fetchContent("../html-component/header.html"),
-   fetchContent("../html-component/footer.html"),
-])
+const baseHref = location.href.split("/").slice(0, -1).join("/");
+const headerPath = `${baseHref}/html-component/header.html`;
+const footerPath = `${baseHref}/html-component/footer.html`;
+
+Promise.all([fetchContent(headerPath), fetchContent(footerPath)])
    .then(([headerContent, footerContent]) => {
       header.innerHTML = headerContent;
       footer.innerHTML = footerContent;
 
       // Nạp tệp header.js sau khi header.html đã được chèn vào
       const scriptElement = document.createElement("script");
-      scriptElement.src = "/assets/js/header.js";
+      scriptElement.src = ".../assets/js/header.js";
       document.body.appendChild(scriptElement);
    })
    .catch((error) => {
